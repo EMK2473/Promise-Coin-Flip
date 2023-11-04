9bb609a0cc912807a2b052b62e1d01a0dc9c8bec
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-04 14:59:45 -0400

    messages edited

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-
 # Promise Coin Flip
+
 ![Github license](https://img.shields.io/badge/mit-blue.svg)
- 
+
 Licensing: [mit](https://choosealicense.com/licenses/mit/)
-    
+
 ## Table of Contents
-  
+
 - [Description](#description)
 - [Installation](#installation-requirements)
 - [Usage](#application-usage)
@@ -15,26 +15,31 @@ Licensing: [mit](https://choosealicense.com/licenses/mit/)
 - [Video Link](#link-to-video-instructions)
 - [Screenshot](#screenshot)
 - [Questions](#questions)
-  
+
 ## Description
+
 A demonstration of how 'promises' work using a coin flip and 'Uber' for the example.
-  
+
 ## Installation Requirements
+
 inquirer@8.2.4
-  
+
 ## Application Usage
+
 Use this application by reviewing the code and run the command line to visually see how a promise works. Promises are dependent on much more than a coin flip, but for this example's purposes we leave it to chance!
-    
+
 ## Contributions
+
 Eric Keeton
-  
+
 ## Test Commands
+
 node index.js
-  
+
 ## Screenshot
+
 ![screenshot of application](./assets/promiseScreenShot.png)
-  
+
 ## Questions
+
 For Questions, contact me at emk2473@gmail.com or visit My Github[EMK2473](https://github.com/EMK2473)
-  
-  
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { default: inquirer } = require("inquirer");
 const callUber = () => {
-  console.log("Hello!\r\n I would like an Uber. Here is my phone number. \r\n If the coin is heads, they will pick me up \r\n If the coin is tails, they can't pick me up.\r\n Then I will call again!");
+  console.log("Hello!\r\nI would like an Uber. Here is my phone number.\r\n Promise you will call me back? \r\n   If the coin lands on heads, then they will pick me up. \r\n   If the coin lands on tails, then they can't pick me up and\r\n   we will try to find a new driver!");
 };
 const waitForUberConfirmation = () =>
   new Promise((resolve, reject) => {
@@ -8,19 +8,19 @@ const waitForUberConfirmation = () =>
       const randNum = Math.floor(Math.random() * 10);
       if (randNum <= 4) {
         resolve(
-          `Heads! Yes, we have an uber on the way.`
+          `Here's the callback we promised: Heads! Yes, we have an uber on the way.`
         );
       } else {
         reject(
           Error(
-            `Tails! Sorry! We are full. No uber for you. Try another driver!`
+            `Here's the callback we promised: Tails! Sorry, we didn't find a driver. We will ask another driver!`
           )
         );
       }
     }, 6000);
   });
 const continueScrolling = () => {
-  setTimeout(() => console.log("I can continue working while I wait"), 2000);
+  setTimeout(() => console.log("I can continue doing whatever I was doing\r\n   while I wait for a returned promise"), 2000);
 };
 const keepCalling = () => {
   callUber();
